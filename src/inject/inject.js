36faf461b5aa082c569368751586d9d37f2d348b
Raymond May Jr <raymondmay@gmail.com>
2013-11-17 09:29:25 +0100

    0.0.3

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,6 +1,9 @@
 /* global $ */
 'use strict';
 
+var _loadedURLS = {};
+var _loadingURLS = {};
+
 function getGifs (context) {
 
   var gifs = [];
@@ -19,38 +22,46 @@ function getGifs (context) {
 
 function loadGif (gif) {
 
-  console.log('loadGif');
-
   var isLoaded = false;
   var url = gif.src;
-  var image = new Image();
   var $loading = $('<div class="gif-loading">Loading yer gif ...</div>');
-
   var $gif = $(gif).addClass('gif-delayer');
 
   function loaded () {
 
     if (!isLoaded) {
-      console.log('#loaded');
       isLoaded = true;
       $loading.remove();
       $gif.addClass('loaded');
+      _loadedURLS[url] = true;
+      _loadingURLS[url].resolve();
     }
   }
 
-  $gif.after($loading);
+  if (!_loadedURLS[url] && !_loadingURLS[url]) {
 
-  image.addEventListener('load', loaded);
-  image.src = url;
-  if (image.complete) {
-    loaded();
+    _loadingURLS[url] = new $.Deferred();
+
+    $gif.after($loading);
+    var image = new Image();
+    image.addEventListener('load', loaded);
+    image.src = url;
+    if (image.complete) {
+      loaded();
+    }
+  }
+  else {
+    $.when(_loadingURLS[url]).then(function () {
+
+      loaded();
+    });
   }
 }
 
 function loadGifs (context) {
 
   var gifs = getGifs(context);
-  gifs.length && console.log('loadGifs', gifs.length);
+  // gifs.length && console.log('loadGifs', gifs.length);
   for (var i=0, len=gifs.length; i<len; i++) {
     loadGif(gifs[i]);
   }
@@ -65,13 +76,19 @@ function observe () {
   var observer = new MutationObserver(function (mutations) {
 
     var node;
-
     mutations.forEach(function (mutation) {
     
-      if (mutation.addedNodes) {
+      if (mutation.addedNodes && mutation.addedNodes.length) {
+
         for (var i=0, len=mutation.addedNodes.length; i<len; i++) {
           node = mutation.addedNodes[i];
           loadGifs(node);
+        }
+      }
+      else if (mutation.attributeName === 'class'){
+
+        if (!$(mutation.target).hasClass('gif-delayer')) {
+          loadGifs(mutation.target);
         }
       }
     });
@@ -80,8 +97,8 @@ function observe () {
   // configuration of the observer:
   var config = {
 
-    'childList': true, //Set to true if mutations to target's children are to be observed.
-    // attributes  Set to true if mutations to target's attributes are to be observed.
+    'childList': true, // Set to true if mutations to target's children are to be observed.
+    'attributes': true, // Set to true if mutations to target's attributes are to be observed.
     // characterData Set to true if mutations to target's data are to be observed.
     'subtree': true, // Set to true if mutations to not just target, but also target's descendants are to be observed.
     // attributeOldValue Set to true if attributes is set to true and target's attribute value before the mutation needs to be recorded.
